9f77204ba7694248ae5caa18d5d0cd84221f4bb6
Emerson Felipe <felipemartins@gmail.com>
2022-06-02 22:36:24 -0300

    Exercicios de classes

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int cauculo = 10;
+		int numeroA = 35;
+		
+		double calculo = 2.5;
+		int resultado = cauculo+numeroA;
+		// TODO Auto-generated method stub
+		System.out.println("Bem vindo a pasta de teste");
+		System.out.println("O resultado é: "+ resultado); 
+		
+		
+
+	}
+
+}
